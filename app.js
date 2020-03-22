bf016f044a3236c1206aec7b24d0abc893ee7f39
Micah Elijah <melijah200@gmail.com>
2020-03-23 00:13:03 +0100

    Added new feature, now country stats on the covid19 is available

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express"),
-            ejs          = require("ejs")
+            ejs          = require("ejs"),
+            request = require("request"),
+            bodyParser = require("body-parser")
 
 const app = express()
 app.use(express.static("public"))
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get("/", function(req,res){
@@ -20,7 +23,37 @@ app.get("/report", function(req,res){
     res.render("report")
 })
 
+app.get("/stats", function(req, res){
+    var request = require("request");
+    var options = {
+    method: 'GET',
+    url: 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php',
+    qs: {country: 'Nigeria'},
+    headers: {
+        'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
+        'x-rapidapi-key': 'ce6eed163cmshe72597862eb749cp10932ejsn8209333a4ca5'
+    }
+    };
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log(error)
+        }else{
+            newBody = JSON.parse(body)
 
-app.listen(process.env.PORT || 5000, function(){
+            var nigeria = newBody.countries_stat[110]
+            var ghana = newBody.countries_stat[116]
+            var benin = newBody.countries_stat[165]
+            var chad = newBody.countries_stat[177]
+            var niger = newBody.countries_stat[173]
+            var gambia = newBody.countries_stat[181]
+            
+            const countries = [nigeria, ghana, benin, chad, niger, gambia]
+            res.render("stats", {countries: countries})
+        }
+    });
+})
+
+
+app.listen(process.env.PORT || 4000, function(){
     console.log("let's save the world >>>")
 })
